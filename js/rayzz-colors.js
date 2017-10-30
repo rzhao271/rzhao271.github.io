9c79a42090563248521b6ae79d37d154fbf3bd36
sharkretriever <thermostatico@gmail.com>
2017-10-30 00:25:36 -0400

    Revamp site theme and remove old pages

--- js/rayzz-colors.js
+++ js/rayzz-colors.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 function prepareColors(hour) {
   var numColorBars = 3;
-  if (hour <= 6 || hour >= 18) {
+  if (hour <= 7 || hour >= 17) {
     for (var i = 1; i <= numColorBars; i++) {
       $(".color-bar-" + i).addClass("color-bar-" + i + "-night");
       $("a").addClass("link-night");
